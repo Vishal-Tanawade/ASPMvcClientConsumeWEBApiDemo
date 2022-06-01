43978fc4d43def606fb40a2a3ec4c985e3b43550
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-01 07:27:25 +0530

    Now run project => data will be fetached from web api=> sql

--- ASPMvcClientConsumeWEBApiDemo/Startup.cs
+++ ASPMvcClientConsumeWEBApiDemo/Startup.cs
@@ -46,7 +46,7 @@ namespace ASPMvcClientConsumeWEBApiDemo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=ComputerProducts}/{action=Index}/{id?}");
             });
         }
     }
