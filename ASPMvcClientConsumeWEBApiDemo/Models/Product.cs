826d7e1959c00aa5795e8754cf0d93cd3b56239e
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-01 07:07:21 +0530

    Poco class added

---
+++ ASPMvcClientConsumeWEBApiDemo/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPMvcClientConsumeWEBApiDemo.Models
+{
+    public class Product
+    {
+        //Serialization: ???
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductCode { get; set; }
+        [Required(ErrorMessage = "Product Name Should not be blank")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "Product price should not be blank")]
+        public decimal ProductPrice { get; set; }
+    }
+}
